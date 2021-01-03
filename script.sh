91a03f26f807a064f761df268517ffa034231a44
ralevn <niki_ralev@hotmail.com>
2021-01-03 19:38:52 +0200

    back to last change

--- script.sh
+++ script.sh
@@ -1,12 +1,8 @@
 #! /bin/sh
 
 nam=$(hostname)
-<<<<<<< HEAD
-echo "Yor computer name is $nam"
-=======
 
 echo "Yor computer name is  : $nam"
 echo "You are logged as     : $USER"
->>>>>>> master
 
 
