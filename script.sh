a1a9e7558a840f1d9ca0505311caa5f4d5cb74f9
ralevn <niki_ralev@hotmail.com>
2021-01-03 22:05:29 +0200

    3d attempt

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@ echo "Your computer name is  : $(hostname)"
 echo "You are logged as      : $USER"
 echo "Your process is        : $$"
 echo "Your home is           : $HOME"
+echo "Your shell is          : $SHELL"
 
 
 
