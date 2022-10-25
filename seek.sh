0dd9fb29ffed25ea0e4229e94fc9ada6598cec79
ralevn <niki_ralev@hotmail.com>
2022-10-25 11:32:39 +0300

    Improved seek.sh parameter processing

--- seek.sh
+++ seek.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 -o ! -d $1 ]; then
+if [ $# -lt 2 ]; then
 	echo -e "Usage: seek.sh <path> <search_string> [-print]\n \
 		 -print: will print files found"
 	exit
 fi
 
+toprint=False
 
-FSTRING=$2
-FPATH=$1
+while [ "$1" != "" ]; do
+	if [ -d $1  ]; then 
+	       FPATH=$1
+           elif [ $1 == "-print" -o $1 == "-p"  ]; then
+               toprint=True
+           else
+               FSTRING=$1		
+        fi
+        shift
+done
+
+echo -e "Search Path: $FPATH \nPrint files: $toprint \nSearched string: $FSTRING\n"
+
 underlin="================================="
 
-if [ $3 ]; then
+if [ $toprint == "True" ]; then
 	find $FPATH -type f  -exec grep -qI $FSTRING {} \; -exec echo -e "\nFILE: {}:\n$underlin" \; -exec cat {} \;
 else
 	find $FPATH -type f  -exec grep -qI $FSTRING {} \; -print
