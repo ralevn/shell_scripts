abb8cde697c8be4443d3f8be2a9932e86df7444c
ralevn <niki_ralev@hotmail.com>
2022-10-25 14:15:57 +0300

    runremotecmd almost finished Still Issues with "Activate the web console with"

--- runremotecmd.sh
+++ runremotecmd.sh
@@ -11,8 +11,21 @@ HOSTS_FILE=$1
 CMD_FILE=$2
 
 mapfile -t HOSTS < $HOSTS_FILE
+mapfile CMDS < $CMD_FILE
+
+
+EXECF () {
+	host=$1
+	while read cmd
+	do
+		echo "COMMAND: $WHITE$cmd $GREY"
+		ssh $host 2>/dev/null <<< $cmd
+		echo "$NC"
+	done <<< ${CMDS[*]}
+	}
 
 for hst in $HOSTS
 do 
-	ssh $hst "bash -s" < $CMD_FILE
+	EXECF $hst 2>/dev/null|grep -v "cockpit.socket"
 done
+
