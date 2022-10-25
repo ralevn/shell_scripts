359da554b58f1ed22892f1f89872040d29a14598
ralevn <niki_ralev@hotmail.com>
2022-10-25 18:54:12 +0300

    acceptable changes for runremote.cmd (mapfile is stupid)

--- runremotecmd.sh
+++ runremotecmd.sh
@@ -5,23 +5,24 @@ CYAN=$'\e[0;36m'
 BLUE=$'\e[0;34m'
 WHITE=$'\e[1;37m'
 GREY=$'\e[0;37m'
+YELLOW=$'\e[0;33m'
+GREEN=$'\e[1;32m'
 NC=$'\e[0m'
 
 HOSTS_FILE=$1
 CMD_FILE=$2
 
 mapfile -t HOSTS < $HOSTS_FILE
-mapfile CMDS < $CMD_FILE
 
 
 EXECF () {
 	host=$1
 	while read cmd
 	do
-		echo "COMMAND: $WHITE$cmd $GREY"
-		ssh $host 2>/dev/null <<< $cmd
+		echo "($GREEN$host :$WHITE: $cmd$NC)$YELLOW"
+		ssh $host 2>/dev/null <<< "$cmd"
 		echo "$NC"
-	done <<< ${CMDS[*]}
+	done < <(grep -Ev "^$|^#" $CMD_FILE)
 	}
 
 for hst in $HOSTS
