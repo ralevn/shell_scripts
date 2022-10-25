ac9f47e06af35ad5fd3492907442d53562f301cb
ralevn <niki_ralev@hotmail.com>
2022-10-25 18:16:01 +0300

    Revert " alabala with runremote"
    This reverts commit d1ccbf899e9b3db86a56597118041ffcfd24befc.

--- runremotecmd.sh
+++ runremotecmd.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash 
 
 RED=$'\e[0;31m'
-YELLOW=$'\e[0;33m'
 CYAN=$'\e[0;36m'
 BLUE=$'\e[0;34m'
 WHITE=$'\e[1;37m'
 GREY=$'\e[0;37m'
-GREEN=$'\e[0;32m'
 NC=$'\e[0m'
 
 HOSTS_FILE=$1
@@ -15,18 +13,19 @@ CMD_FILE=$2
 mapfile -t HOSTS < $HOSTS_FILE
 mapfile CMDS < $CMD_FILE
 
+
 EXECF () {
 	host=$1
 	while read cmd
 	do
-		echo "$GREEN$host :$WHITE: $cmd $YELLOW"
+		echo "COMMAND: $WHITE$cmd $GREY"
 		ssh $host 2>/dev/null <<< $cmd
-		echo -n "$NC"
+		echo "$NC"
 	done <<< ${CMDS[*]}
 	}
 
 for hst in $HOSTS
 do 
-	EXECF $hst 2>/dev/null|grep -Ev "^$|cockpit.socket"
+	EXECF $hst 2>/dev/null|grep -v "cockpit.socket"
 done
 
